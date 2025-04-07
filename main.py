6dbd3d68d642e06df66124482cc7e94c10d3565a
DoPhuc05 <phuc201005@gmail.com>
2025-04-08 01:14:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -173,5 +173,4 @@ def realtime_detection():
     return {"message": "Realtime detection started. Press Q to stop."}
 
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
